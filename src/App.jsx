6d5cc7f5266666ef574438be677c15f7910fb181
naimsheikh8020 <naimsheikh8020@gmail.com>
2025-11-16 03:27:32 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,105 @@
-import React from 'react'
-import Header from './components/Header';
-import Home from "./pages/Home"
-import Features from "./pages/Features"
-import Reviews from "./pages/Reviews"
-import Pricing from "./pages/Pricing"
-import Blog from "./pages/Blog"
-import Trusted from './section/Trusted';
-import Section from './pages/Section';
+// import React from 'react'
+// import Header from './components/Header';
+// import Home from "./pages/Home"
+// import Features from "./pages/Features"
+// import Reviews from "./pages/Reviews"
+// import Pricing from "./pages/Pricing"
+// import Blog from "./pages/Blog"
+// import Trusted from './section/Trusted';
+// import Section from './pages/Section';
 
+// const App = () => {
+//   return (
+//     <div className='bg-white'>
+//       <Header />
+//       <main className="mt-5 md:px-12 sm:px-0">
+//         <section id="home" name="home">
+//           <Home />
+//         </section>
+
+//         <section id="section" name="section">
+//           <Section />
+//         </section>
+
+//         <section id="features" name="features">
+//           <Features />
+//         </section>
+
+//         <section id="reviews" name="reviews">
+//           <Reviews />
+//         </section>
+
+//         <section id="pricing" name="pricing">
+//           <Pricing />
+//         </section>
+
+//         <section id="blog" name="blog">
+//           <Blog />
+//         </section>
+//       </main>
+      
+//     </div>
+//   );
+
+// }
+
+// export default App
+
+
+import React from "react";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Features from "./pages/Features";
+import Reviews from "./pages/Reviews";
+import Pricing from "./pages/Pricing";
+import Blog from "./pages/Blog";
+import Trusted from "./section/Trusted";
+import PricingPage from "./pages/PricingCard";
+import FrequentlyAskedQuestions from "./pages/FrequentlyAskedQuestions";
+import ReadyToAchieve from "./pages/ReadyToAchieve";
+import Footer from "./pages/Footer";
+ 
 const App = () => {
   return (
-    <div className='bg-white'>
+    <div>
       <Header />
       <main className="mt-5 md:px-12 sm:px-0">
         <section id="home" name="home">
           <Home />
         </section>
-
-        <section id="section" name="section">
-          <Section />
-        </section>
-
+ 
         <section id="features" name="features">
           <Features />
         </section>
-
+ 
         <section id="reviews" name="reviews">
           <Reviews />
         </section>
-
+ 
         <section id="pricing" name="pricing">
           <Pricing />
         </section>
-
+ 
         <section id="blog" name="blog">
           <Blog />
         </section>
+        <section id="pricing" name="pricing">
+          <PricingPage />
+        </section>
+        <section id="FrequentlyAskedQuestions" name="FrequentlyAskedQuestions">
+          <FrequentlyAskedQuestions />
+        </section>
+        <section id="ReadyToAchieve" name="ReadyToAchieve">
+          <ReadyToAchieve />
+        </section>
+        <section id="footer" name="footer">
+          <Footer />
+        </section>
       </main>
-      
     </div>
   );
-
-}
-
-export default App
+};
+ 
+export default App;
+ 
+ 
