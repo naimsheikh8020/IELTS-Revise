633ba0e4d6f540342789134bddf7bb0a2e0cb665
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-11-16 04:07:36 +0600

    refactor: simplify layout structure in Features component

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -7,7 +7,7 @@ import Student_Result from "../section/Student_Result";
 const Features = () => {
   return (
     <div className="">
-      <div className="p-6 mt-2 sm:p-8 lg:p-10">
+      <div className="">
         <p className="bg-blue-100 text-base my-5 text-blue-600 px-4 py-2 rounded-full text-center mx-auto block w-fit">
   What Make Us different
 </p>
@@ -17,7 +17,7 @@ const Features = () => {
         <p className="text-gray-500 font-normal mt-3 sm:mt-4 text-sm sm:text-base text-center px-4">
           Professional features designed by certified IELTS examiners to maximize your score
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-6 px-6 py-12">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-6  container mx-auto py-12">
           <PracticeCard
             icon={assets.authentic}
             title="Exam-Authentic Questions"
